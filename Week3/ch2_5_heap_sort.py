5b8d0e7e5d421fbe7e2f6bb2b457ff066bb3702c
Gihyeon <hamzzi99@naver.com>
2025-09-27 21:22:06 +0700

    ch25hs-2. Heapify 를 구현하고 1칸 진행한다
    코드 다시 재작성

--- Week3/ch2_5_heap_sort.py
+++ Week3/ch2_5_heap_sort.py
@@ -1,20 +1,20 @@
 from time import time
 
 def heapify(root, size):
-  lc = root * 2 + 1      
+  lc = root * 2 + 1
   if lc >= size: return
   child = lc
-  rc = root * 2 + 2      
-  if rc < size:         
+  rc = root * 2 + 2
+  if rc < size:
     vis.compare(rc, lc)
-    if array[rc] > array[lc]: 
-      child = rc              
+    if array[rc] > array[lc]:
+      child = rc
 
   vis.compare(root, child)
-  if array[root] < array[child]:  
+  if array[root] < array[child]:
     vis.swap(root, child)
-    array[root], array[child] = array[child], array[root] 
-    heapify(child, size) 
+    array[root], array[child] = array[child], array[root]
+    heapify(child, size)
 
 def main():
   print('before:', array)
