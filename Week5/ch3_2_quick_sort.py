061753a264b2b5f45757c1ed673871de8309a293
Gihyeon <hamzzi99@naver.com>
2025-10-05 10:59:33 +0900

    ch32qs-13. 남은 원소가 2개뿐일 때는 직접비교하는 버전으로 구현해 본다 (+Visualizer)

--- Week5/ch3_2_quick_sort.py
+++ Week5/ch3_2_quick_sort.py
@@ -11,20 +11,20 @@ def main():
 
   # print('after :', array)
 
-def quickSort(left, right): #q=inclusive
-  if left == right: vis.fix(left)  # 정렬 대상이 하나뿐이라면 확정해도 좋다
-  if left >= right: return         # 정렬할 것이 없으면 할 일이 없다
+def quickSort(left, right):
+  if left == right: vis.fix(left)
+  if left >= right: return
+  if right == left + 1:
+    # vis.compare(left, right)
+    if array[left] > array[right]:
+      # vis.swap(left, right)
+      array[left], array[right] = array[right], array[left]
+    return  
   # if right < left + 4:
   #   # insertionSort(left, right)
   #   return
-  vis.push(left, right)
-  pivot = partition(left, right)   # pivot 위치를 결정해 온다
-  vis.set_pivot(pivot)
-  quickSort(left, pivot-1)  # pivot 보다 왼쪽 그룹을 다시 quickSort 한다
-  quickSort(pivot+1, right) # pivot 보다 오른쪽 그룹을 다시 quickSort 한다
-  vis.pop()
 
-def insertionSort(left, right): #right=inclusive
+def insertionSort(left, right):
   for i in range(left + 1, right + 1):
     v = array[i]
     vis.mark_end(i, v)
@@ -38,19 +38,18 @@ def insertionSort(left, right): #right=inclusive
 
 def partition(left, right):
 
-  pi = left               # pi = Pivot Index
-  pivot = array[pi]       # pivot = Pivot Value
+  pi = left
+  pivot = array[pi]
 
-  p, q = left, right + 1  # 후보선수들 출전준비
+  p, q = left, right + 1
 
-  while True:             # p < q 인 동안 하게 된다. 하지만 중간에 break 하므로 while True 를 쓰자
-    while True:           # 왼쪽에서 pivot 보다 큰 값을 찾을때까지
+  while True:
+    while True:
       p += 1
       vis.set_p(p)
       if q < p: break
       if p <= right: vis.compare(pi, p)
       if p > right or array[p] > pivot: break 
-      # 왼쪽에서 pivot 보다 큰 값을 찾았다
 
       if p <= right: vis.set_left(p)
 
