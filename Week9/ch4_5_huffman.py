e4faa184da02cad11f7f633bf23914946fa983f5
Gihyeon <hamzzi99@naver.com>
2025-11-09 08:33:02 -0600

    ch45hu-7. tree 모양으로 출력.

--- Week9/ch4_5_huffman.py
+++ Week9/ch4_5_huffman.py
@@ -47,3 +47,12 @@ while len(nodes) > 1:
   heappush(nodes, node)
 
 print(nodes)
+
+def printTree(node, indent=0):
+  print(f'{" " * indent}{node.freq}/{node.ch}')
+  if node.left is not None:
+    printTree(node.left, indent+2)
+  if node.right is not None:
+    printTree(node.right, indent+2)
+
+printTree(nodes[0])
