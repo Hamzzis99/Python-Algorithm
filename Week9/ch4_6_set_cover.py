fc2c408b109fc76f67f734b853f4b348e62b3ac3
Gihyeon <hamzzi99@naver.com>
2025-11-09 08:37:37 -0600

    ch46sc-10. Graph 에서의 Set Cover - City 데이터 준비하기

--- Week9/ch4_6_set_cover.py
+++ Week9/ch4_6_set_cover.py
@@ -1,64 +1,56 @@
-from pyvisalgo import SetCoverVisualizer as Visualizer
+from pyvisalgo import CitySetCoverVisualizer as Visualizer
+import data_sample_cities as dsc
 from copy import deepcopy
-from random import randint, seed
 
-# Set Cover 시작
-u={1,2,3,4,5,6,7,8,9,10}
-f=[
-  {1,2,3,8},
-  {1,2,3,4,8},
-  {1,2,3,4},
-  {2,3,4,5,7,8},
-  {4,5,6,7},
-  {5,6,7,9,10},
-  {4,5,6,7},
-  {1,2,4,8},
-  {6,9},
-  {6,10},
-]
+def build_graph():
+  global graph
+  graph = {u: dict() for u in range(n_cities)}
+  for u,v,w in edges:
+    graph[u][v] = w
+    graph[v][u] = w
 
 def main():
-  C = [] # 결과를 저장할 배열
+  global n_cities
+  n_cities = len(cities)
 
-  global U
+  build_graph()
+
+  global u, f, U, F
+  f = [
+    set(list(d.keys()) + [u]) for u,d in graph.items()
+  ]
+  print(f)
+  U = deepcopy(u)
+  F = deepcopy(f)
+  vis.draw()
+
+  return
+  vis.wait(1000)
+
+  global C
+  C = []
   while U:
-    vis.count_elements()
     max_i = F.index(max(F, key=lambda s: len(s & U)))
     vis.fix(max_i)                 # max_i 번째에 가장 원소가 많이 겹친다
-
-    U -= F[max_i]    # U 에서 가장 많이 겹치는 그룹의 원소를 제거한다
-    S = F.pop(max_i) # F 에서 해당 subset 집합을 제거하고
-    # S = F[max_i]
-    # F[max_i] = set()
-    C.append(S)      # 결과 배열인 C 에 넣는다
-    print(f'{C=}')
-
-seed('SetCover')
-vis = Visualizer('Set Cover - Simple Set')
-while True:
-  U = deepcopy(u) # 원본에서 제거하면서 진행할 것이므로
-  F = deepcopy(f) # 복사를 해 둔다
-  vis.setup(vis.get_main_module())
-  vis.reset()
-  main()
+    S = F[max_i] # F 에서 U 와의 교집합이 가장 큰 부분집합
+    U -= S       # U 에서 해당 부분집합의 원소를 제거한다
+    F[max_i] = set()
+    C.append(S)
+    print(f'{U=}, {C=}')
   vis.draw()
-  again = vis.end()
-  if not again: break
 
-  n_cities = randint(15, 30)
-  print(f'{n_cities=}')
-  u = set()
-  f = []
-  si = 0
+
+if __name__ == '__main__':
+  vis = Visualizer('Set Cover - Cities')
   while True:
-    s = set()
-    sc = randint(2, 12)
-    for i in range(sc):
-      ci = (si + randint(1, 12)) % n_cities
-      s.add(ci)
-    u |= s
-    f.append(s)
-    # print(s)
-    if len(u) == n_cities: break
-
-    si = (si + 1) % n_cities
+    cities, edges = dsc.cities, dsc.edges
+    u = set(range(0, len(cities)))
+    vis.setup(vis.get_main_module())
+    vis.draw()
+    main()
+    again = vis.end()
+    if not again: break
+    if vis.restart_lshift:
+      dsc.next()
+    elif vis.restart_rshift:
+      dsc.random()
