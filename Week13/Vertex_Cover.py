8950e7a78a37785c265e5c658fec43fe340d07bc
Gihyeon <hamzzi99@naver.com>
2025-11-30 22:04:55 +0900

    ch82vc-2. VC: SetCover - Set Cover 알고리즘을 활용하여 Vertex-Edge Cover 를 완료한다

--- Week13/Vertex_Cover.py
+++ Week13/Vertex_Cover.py
@@ -4,42 +4,56 @@ import data_sample_cities as dsc
 
 
 class VertexCover:
-  def __init__(self, cities, edges, usingSetCover=True):
-    self.cities = cities
-    self.edges = edges
-    self.usingSetCover = usingSetCover
-    self.main = self.setCoverMain if usingSetCover else self.maxMatchMain
+    def __init__(self, cities, edges, usingSetCover=True):
+        self.cities = cities
+        self.edges = edges
+        self.usingSetCover = usingSetCover
+        self.main = self.setCoverMain if usingSetCover else self.maxMatchMain
+        self.build_graph()
 
-  def setCoverMain(self):
-    print('Using Set Cover')
-    n_cities = len(self.cities)
-    n_edges = len(self.edges)
-    self.u = { i for i in range(n_edges) }
-    self.f = [ set() for _ in range(n_cities) ]
-    for i in range(n_edges):
-      u,v,w = self.edges[i]
-      self.f[u].add(i)
-      self.f[v].add(i)
-    print(self.u, self.f)
-    self.U = deepcopy(self.u)
-    self.F = deepcopy(self.f)
-    vis.draw()
+    def setCoverMain(self):
+        print('Using Set Cover')
+        n_cities = len(self.cities)
+        n_edges = len(self.edges)
+        self.u = {i for i in range(n_edges)}
+        self.f = [set() for _ in range(n_cities)]
+        for i in range(n_edges):
+            u, v, w = self.edges[i]
+            self.f[u].add(i)
+            self.f[v].add(i)
+        print(self.u, self.f)
+        self.U = deepcopy(self.u)
+        self.F = deepcopy(self.f)
 
-  def maxMatchMain(self):
-    print('Using Maximul Matching')
-    vis.draw()
+        self.C = []
+        while self.U:
+            max_i = self.F.index(max(self.F, key=lambda s: len(s & self.U)))
+            vis.fix(max_i)
+            print(f'fixing {max_i}')
+            S = self.F[max_i]
+            self.U -= S
+            print(S, self.U, self.F)
+            self.F[max_i] = set()
+            self.C.append(S)
+
+        vis.draw()
+
+    def maxMatchMain(self):
+        print('Using Maximul Matching')
+        vis.draw()
+
 
 vis = Visualizer('Vertex Cover')
 usingSetCover, gen = True, True
 while True:
-  vc = VertexCover(dsc.cities, dsc.edges, usingSetCover)
-  vis.setup(vc)
-  vc.main()
-  again = vis.end()
-  if not again: break
-  if vis.restart_lshift:
-    dsc.next()
-  elif vis.restart_rshift:
-    dsc.random()
-  else:
-    usingSetCover = not usingSetCover
+    vc = VertexCover(dsc.cities, dsc.edges, usingSetCover)
+    vis.setup(vc)
+    vc.main()
+    again = vis.end()
+    if not again: break
+    if vis.restart_lshift:
+        dsc.next()
+    elif vis.restart_rshift:
+        dsc.random()
+    else:
+        usingSetCover = not usingSetCover
